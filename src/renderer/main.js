3834c10a7ba36fdc18e8024e5e84010ef8783a39
DorofeevMark <creekcreek760@gmail.com>
2019-04-08 19:29:00 +0300

    Initiated vue with submodule

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
-
-import App from './App'
-import router from './router'
-import store from './store'
+import App from '../../hawk.garage/src/App.vue'
+import router from '../../hawk.garage/src/router'
+import store from '../../hawk.garage/src/store'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
@@ -11,8 +10,7 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
   router,
   store,
-  template: '<App/>'
+  render: h => h(App)
 }).$mount('#app')
