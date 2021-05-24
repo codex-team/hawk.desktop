e5fad509b595824914fe193cf9350e5703a6c389
Taly <vitalik7tv@yandex.ru>
2021-05-24 18:34:33 +0300

    Define min window size

--- src/main.js
+++ src/main.js
@@ -12,7 +12,9 @@ let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
+    minWidth: 1200,
     height: 800,
+    minHeight: 800,
     webPreferences: {
       nodeIntegration: true,
     },
