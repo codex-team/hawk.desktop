08b8f82b3fcbde3b76ab54b038c6a5e63b8d5565
ilyamore88 <themorozik@gmail.com>
2021-07-15 19:34:58 +0300

    fix renaming

--- src/protocols.js
+++ src/protocols.js
@@ -4,14 +4,13 @@ const path = require('path');
 module.exports = () => {
   // Used for redirecting request to index.html and handle static assets
   protocol.interceptFileProtocol('file', (req, callback) => {
-    console.log(req.url);
     let url = req.url.substr(8); // cut file://
 
     /** Remove path to magic root dir from url */
     url = url.replace(path.parse(url).root, '');
 
-    /** If no 'static' at the start of path then return index.html */
-    if (!/^static/.test(url)) {
+    /** If file url is empty, load index.html file */
+    if (url === '') {
       url = '/index.html';
     }
 
