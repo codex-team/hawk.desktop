27bc1a2447fdd5a88a41fa17107cb229f1be8c71
Taly <vitalik7tv@yandex.ru>
2021-07-22 20:12:06 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ function createWindow() {
   // Set application menu
   Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
 
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 }
 
 /**
